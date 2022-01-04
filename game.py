24ef698b1edb70a1ea4d6f84977e327e6fc6a8d8
stormy_ <stormy.hs@protonmail.com>
2022-01-04 23:16:45 +0100

    big cleanup

--- game.py
+++ game.py
@@ -1,10 +1,10 @@
-import random, sys, time
+import random
+import sys
+import time
 from dataclasses import dataclass
 
 '''
-Stormy & Sl*v License
-
-Copyright (c) 2021 Adolf and Stormy.Inc
+Copyright (c) 2021 Adolf and Stormy
 
 Permission is hereby NOT granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -27,68 +27,68 @@ SOFTWARE.
 Stealing niggas gotta pay royalties
 '''
 
+
 def askPlayer(question):
     answer = input(question)
     return answer.lower().strip()
 
+
 def coolTransition():
+    time.sleep(0.25)
     print(".")
-    time.sleep(0.5)
+    time.sleep(0.25)
     print(".")
-    time.sleep(0.5)
+    time.sleep(0.25)
     print(".")
-    time.sleep(0.5)
+    time.sleep(0.25)
 
-class entity:
-    name = "0"
-    health = 100
-    strenght = 10
-    speed = 10
-    xp = 0
-    energy = 100
 
-def calculateAttack(attackType, enemy):
-    if enemy.energy == 0:
-        print("You ran out of energy.")
-        return # OR, continue getting raped
+class entity():
+    def __init__(self, name="Default Entity", health=100, strength=10, speed=10, xp=0, energy=100):
+        self.name = name
+        self.health = health
+        self.strength = strength
+        self.speed = speed
+        self.xp = xp
+        self.energy = energy
 
-    attackTypeDmg1 = 0
-    totalDmg = 0                  
-    
-    if attackType == "hard attack":
-        attackTypeDmg = random.randint(5,10) + enemy.strenght
-        enemy.health -= attackTypeDmg
-        print(f"{enemy.name} took {attackTypeDmg} damage!")
-        totalDmg += attackTypeDmg + attackTypeDmg1
-        print(f"Total damage dealt: {totalDmg}") 
-        enemy.energy -= 100
 
-    elif attackType == "fast attack":
-        attackTypeDmg = random.randint(1,7) + enemy.strenght
-        enemy.health -= attackTypeDmg
-        print(f"{enemy.name} took {attackTypeDmg} damage!")
-        enemy.energy -= 50
+player = entity()
+player.name = input("Enter your name: ").strip()
+print(f"Welcome, {player.name}.")
 
+ilkhar = entity("Ilkhar", strength=5, speed=20, xp=100)
+
+
+def calculateAttack(enemy):
+    print(f"{player.name} IS ATTACKING")
+    while (player.energy > 0):
+        time.sleep(0.1)
         attackType = askPlayer("Pick an attack: (hard attack/fast attack) : ")
 
-        if attackType == "fast attack":
-            attackTypeDmg1 = random.randint(1,7) + enemy.strenght
-            enemy.health -= attackTypeDmg1
-            print(f"{enemy.name} took {attackTypeDmg} damage!")
-            totalDmg += attackTypeDmg + attackTypeDmg1
-            print(f"Total damage dealt: {totalDmg}")
-            enemy.energy -= 50
+        attackTypeDmg1 = 0
+        totalDmg = 0
 
-    if attackType == "hard attack":
-        if(enemy.energy < 100):
-            print("Too tired to do a hard attack now.")
-        else:
-            attackTypeDmg = random.randint(5,10) + enemy.strenght
+        if attackType == "hard attack":
+            if(player.energy < 100):
+                print("Too tired to do a hard attack now.")
+            else:
+                attackTypeDmg = random.randint(5, 10) + enemy.strength
+                enemy.health -= attackTypeDmg
+                print(f"{enemy.name} took {attackTypeDmg} damage!")
+                totalDmg += attackTypeDmg + attackTypeDmg1
+                print(f"Total damage dealt: {totalDmg}")
+                player.energy -= 100
+
+        elif attackType == "fast attack":
+            attackTypeDmg = random.randint(1, 7) + enemy.strength
             enemy.health -= attackTypeDmg
             print(f"{enemy.name} took {attackTypeDmg} damage!")
-            totalDmg += attackTypeDmg + attackTypeDmg1
-            print(f"Total damage dealt: {totalDmg}") 
-            enemy.energy -= 100
+            player.energy -= 50
+
+    print(f"{player.name} ran out of energy.")
+    player.energy = 100  # reset every turn?
+
 
 def fight(enemy):
     print(f"Battling {enemy.name}!")
@@ -96,8 +96,16 @@ def fight(enemy):
 
     fightOver = False
     while fightOver == False and player.health > 1 and enemy.health > 1:
-        attackType = askPlayer("Pick an attack: (hard attack/fast attack) : ")
-        calculateAttack(attackType, enemy)
+        # Player attack
+        coolTransition()
+        calculateAttack(enemy)
+        # Enemy attack
+        coolTransition()
+        print(f"{enemy.name} IS ATTACKING")
+        time.sleep(0.1)
+        player.health -= enemy.strength
+        print(f"{player.name} took {enemy.strength} damage!")
+
         if player.health == 0 or enemy.health == 0:
             fightOver = True
 
@@ -110,20 +118,6 @@ def fight(enemy):
         print(f"{enemy.name} is dead. {player.name} has gained {enemy.xp} experience.")
         player.xp += enemy.xp
 
-player = entity()
-player.name = input("Enter your name: ").strip()
-print(f"Welcome, {player.name}.")
-
-enemy = entity()
-ilkhar = enemy()
-ilkhar.name = "Ilkhar"
-ilkhar.strenght = 5
-ilkhar.speed = 20
-ilkhar.xp = 100
-
-if player.energy == 0:
-    print(f"{enemy}'s turn.")
-    
 
 if askPlayer("Do you wish to start the game? (yes/no): ") == 'yes':
     print("You are a random ass faggot adventurer going down a gay ass road")
@@ -133,7 +127,8 @@ if askPlayer("Do you wish to start the game? (yes/no): ") == 'yes':
         print("An Ilkhar jumps out of the shrubbery with an intent to rape.")
         time.sleep(1)
         if(ilkhar.speed > player.speed):
-            print("It appears the Ilkhar is faster. Running away will probably end in disaster.")
+            print(
+                "It appears the Ilkhar is faster. Running away will probably end in disaster.")
             time.sleep(1)
         if askPlayer("Do you engage in combat or run away? (combat/run): ") == 'combat':
             coolTransition()
@@ -142,14 +137,14 @@ if askPlayer("Do you wish to start the game? (yes/no): ") == 'yes':
             time.sleep(1)
             ilkhar.name = "Ilkhar"
             fight(ilkhar)
-        else: # runs away
+        else:  # runs away
             coolTransition()
             time.sleep(1)
             print("Ilkhar catches up to you and rapes you")
             time.sleep(1)
             print("Game over")
             sys.exit()
-    else: # right
+    else:  # right
         coolTransition()
         print("You keep going down the faggot ass road, when suddenly you encounter a cave entrence!")
         time.sleep(1)
@@ -161,4 +156,3 @@ if askPlayer("Do you wish to start the game? (yes/no): ") == 'yes':
 else:
     print("Nigger cracker kike fag gypsie muzzie chink")
     sys.exit()
-
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Fentanyl Quest
+
+A simple turn-based dungeon explorer, played on your terminal.
+
+## changelog
+- made the code actually run
+- cleaned up random code segments
+- enemies fight back
+- `calculateAttack()` does what it was supposed to do
+- `time.sleep()` calls here and there for less eyerape
+- `coolTransition()` is cleaner
+- other shit i forgot about
