c74d9e3cb05153ac5534085f374c804a80e81d1b
stormy_ <stormy.hs@protonmail.com>
2022-01-05 19:57:27 +0100

    V1.2: cleanup, very basic UI, fix bug where player attack was based on enemy strength

--- game.py
+++ game.py
@@ -5,10 +5,9 @@ from dataclasses import dataclass
 
 '''
 Copyright (c) 2021 Adolf and Stormy
+Notable contributors: TrollerOfHolland
 
-Other contributors: TrollerOfHolland
-
-Permission is hereby NOT granted, free of charge, to any person obtaining a copy
+Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
@@ -25,26 +24,27 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-
-Stealing niggas gotta pay royalties
 '''
+
+debug = False
 
 
 def askPlayer(question):
     answer = input(question)
     return answer.lower().strip()
 
+
 def delayPrint(text, delay):
     print(text)
     time.sleep(delay)
 
 
-def coolTransition(delay = 0):
-    time.sleep(0.25)
-    for i in range(3):
-        delayPrint(".", 0.25)
-
+def coolTransition(delay=0.25):
     time.sleep(delay)
+    for i in range(3):
+        delayPrint(".", delay)
+    time.sleep(delay)
+
 
 class entity:
     def __init__(self, name="Default Entity", health=100, strength=10, speed=10, xp=0, energy=100):
@@ -62,83 +62,109 @@ print(f"Welcome, {player.name}.")
 ilkhar = entity("Ilkhar", strength=5, speed=20, xp=100)
 
 
-def calculateAttack(enemy):
-    print(f"{player.name} IS ATTACKING")
-    while (player.energy > 0):
-        time.sleep(0.1)
+def handleAttack(attacker, defender):
+    time.sleep(0.1)
+    print(f"===\n{attacker.name}: {attacker.health}HP, {attacker.strength}STR")
+    print(f"{defender.name}: {defender.health}HP, {defender.strength}STR\n===")
+    time.sleep(0.1)
+    print(f"{attacker.name} IS ATTACKING")
+    time.sleep(0.2)
+    while (attacker.energy > 0):
+        time.sleep(0.2)
+        time.sleep(0.2)
         attackType = askPlayer("Pick an attack: (hard attack/fast attack) : ")
 
-        attackTypeDmg1 = 0
         totalDmg = 0
-
         if attackType == "hard attack":
-            if(player.energy < 100):
+            if(attacker.energy < 100):
+                time.sleep(0.1)
                 print("Too tired to do a hard attack now.")
             else:
-                attackTypeDmg = random.randint(5, 10) + enemy.strength
-                enemy.health -= attackTypeDmg
-                print(f"{enemy.name} took {attackTypeDmg} damage!")
-                totalDmg += attackTypeDmg + attackTypeDmg1
-                print(f"Total damage dealt: {totalDmg}")
-                player.energy -= 100
+                time.sleep(0.1)
+                attackTypeDmg = random.randint(5, 10) + attacker.strength
+                if(debug):
+                    attackTypeDmg = 50
+                defender.health -= attackTypeDmg
+                print(f"{defender.name} took {attackTypeDmg} damage!")
+                time.sleep(0.1)
+                attacker.energy -= 100
+                time.sleep(0.1)
 
         elif attackType == "fast attack":
-            attackTypeDmg = random.randint(1, 7) + enemy.strength
-            enemy.health -= attackTypeDmg
-            print(f"{enemy.name} took {attackTypeDmg} damage!")
-            player.energy -= 50
+            time.sleep(0.1)
+            attackTypeDmg = random.randint(1, 7) + defender.strength
+            if(debug):
+                attackTypeDmg = 50
+            defender.health -= attackTypeDmg
+            print(f"{defender.name} took {attackTypeDmg} damage!")
+            attacker.energy -= 50
+            time.sleep(0.1)
 
-    print(f"{player.name} ran out of energy.")
-    player.energy = 100  # reset every turn?
+    # out of loop, attacker's turn is done
+    time.sleep(0.15)
+    totalDmg += attackTypeDmg
+    # print("\n")
+    # print(f"{player.name} ran out of energy.")
+    print(f"Total damage dealt: {totalDmg}")
+    player.energy = 100
+
+    if(defender.health > 0):
+        coolTransition(0.25)
+        delayPrint(f"{defender.name} IS ATTACKING", 0.1)
+        while (defender.energy > 0):
+            time.sleep(0.1)
+            attacker.health -= defender.strength
+            print(f"{attacker.name} took {defender.strength} damage!")
+            defender.energy -= 50
+    else:
+        return
+    defender.energy = 100
 
 
 def fight(enemy):
     delayPrint(f"Battling {enemy.name}!", 1)
 
     while True:
-        # Player attack
-        coolTransition()
-        calculateAttack(enemy)
-
-        #checking enemy hp before handling player attacks
-        if(enemy.health < 1): 
-            print(f"{enemy.name} is dead. {player.name} has gained {enemy.xp} experience.")
-            player.xp += enemy.xp
-            break
-
-        # Enemy attack
-        coolTransition()
-        delayPrint(f"{enemy.name} IS ATTACKING", 0.1)
-        player.health -= enemy.strength
-        print(f"{player.name} took {enemy.strength} damage!")
+        coolTransition(0.25)
+        # completes one turn of attack for both args
+        handleAttack(player, enemy)
 
         if(player.health < 1):
             print(f"{player.name} has died.")
             print("Game over.")
+            sys.exit(0)
+            break
+        elif(enemy.health < 1):
+            print(f"{enemy.name} has died. {player.name} has gained {enemy.xp} xp.")
+            player.xp += enemy.xp
             break
 
 
 if askPlayer("Do you wish to start the game? (yes/no): ") == 'yes':
+    coolTransition()
     print("You are a random ass faggot adventurer going down a gay ass road")
     time.sleep(1)
     if askPlayer("You encounter a divergence in the road, do you go left or right? (left/right) : ") == "left":
-        coolTransition()
-        delayPrint("An Ilkhar jumps out of the shrubbery with an intent to rape.", 1)
+        coolTransition(0.25)
+        delayPrint(
+            "An Ilkhar jumps out of the shrubbery with an intent to rape.", 1)
         if(ilkhar.speed > player.speed):
-            delayPrint("It appears the Ilkhar is faster. Running away will probably end in disaster.", 1)
+            delayPrint(
+                "It appears the Ilkhar is faster. Running away will probably end in disaster.", 1)
         if askPlayer("Do you engage in combat or run away? (combat/run): ") == 'combat':
-            coolTransition(delay = 1)
+            coolTransition(0.25)
             delayPrint("Battle begins.", 1)
             ilkhar.name = "Ilkhar"
             fight(ilkhar)
         else:  # runs away
-            coolTransition(delay = 1)
+            coolTransition(0.25)
             delayPrint("Ilkhar catches up to you and rapes you", 1)
             print("Game over")
             sys.exit()
     else:  # right
-        coolTransition()
-        delayPrint("You keep going down the faggot ass road, when suddenly you encounter a cave entrence!", 1)
+        coolTransition(0.25)
+        delayPrint(
+            "You keep going down the faggot ass road, when suddenly you encounter a cave entrence!", 1)
         if askPlayer("Do you enter? (yes/no) : ") == 'yes':
             time.sleep(1)
             delayPrint("A figure shrouded in pure blackness approaches", 1)
--- readme.md
+++ readme.md
@@ -5,17 +5,30 @@ A simple turn-based dungeon explorer, played on your terminal.
 
 ## TODO
 - [ ] Finish kakapoop path
-- [ ] Maybe merge with the original?
+- [ ] Save progress to disk
+- [ ] More story
+
+## Maybe TODO (ideas)
+- [ ] Different classes for each attack?
 
 ## changelog
 
-### v1.1
+### v1.2 (Alpha)
+- `coolTransition()` default value changed
+- `calculateAttack()` switched out for `handleAttack()`, which handles both the player and enemy
+- added prototype stats display for player and enemy during battle
+- added a lot of delays
+- added very basic debug mode
+- player attack damage is no longer based off of the enemy's strength (wtf)
+- unmemed the licence
+
+### v1.1 (Alpha)
 - `delayPrint()` a function to both print and delay for a variable amount of time
 - `coolTransition()` added an optional amount of time to sleep to the after the transition
 - removed 2 useless brackets for maximum efficiency
 - enemy can not attack the player when it is dead
 
-### v1
+### v1 (Alpha)
 - made the code actually run
 - cleaned up random code segments
 - enemies fight back
@@ -24,5 +37,5 @@ A simple turn-based dungeon explorer, played on your terminal.
 - `coolTransition()` is cleaner
 - other shit i forgot about
 
-### v0.1
+### v0.1 (Alpha)
 - Uploaded initial code
